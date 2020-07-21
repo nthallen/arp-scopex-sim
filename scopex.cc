4424a8700a97c93be7842fc9c209c4a86aab7736
Breitenlechner <martinb@seas.harvard.edu>
2020-07-21 13:09:29 -0400

    Martin's changes

--- scopex.cc
+++ scopex.cc
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include <math.h>
 #include <unistd.h>
+
+// for timer and filestream:
+#include <chrono>
+#include <thread>
+#include <fstream>
+
 #include "SCoPEx.h"
 #include "ode/ode.h"
 #include "ode/mass.h"
@@ -35,18 +41,22 @@ SCoPEx::SCoPEx() {
   ductHeightRatio = 0.47;
   ductDischargeRate = 0;
   HeliumMass = 150; // kg
-  initialAltitude = 0; // m
+  initialAltitude = 19999; // m
   // balloonAltitude = 0.0; // m
   // balloonArea = pi * balloonRadius * balloonRadius; // m^2 Derived
   payloadMass = 590.0; // Kg (reduced by tetherMass)
   payloadSize[0] = 1; // m
   payloadSize[1] = 1;
   payloadSize[2] = 1;
+  motorAxisPosition[0] = 1.5; // left/right
+  motorAxisPosition[1] = 0.5; // forward
+  motorAxisPosition[2] = 0; // up
+  
   payloadArea = payloadSize[0]*payloadSize[2]; // X x Z, assumes motion in Y direction only
   payloadCd = 1.05; // assumes motion in Y direction only
   tetherMass = 10; // Kg
   tetherRadius = 0.02;
-  tetherLength = 2*balloonMaxHeight;  // length
+  tetherLength = 100;  // length
   thrust = 4.;
   thrustIncrement = 0.25;
   direction = 90.; // +Y [-180, 180]
@@ -72,6 +82,12 @@ SCoPEx::SCoPEx() {
   velocityAngleIntegral = 0;
   gondolaAngleIntegral = 0;
   gondolaAngleIntegralLimit = 0.25;
+
+  // Windshear:
+  WindNorthBalloon = 0; // m/s
+  WindEastBalloon = 0;
+  WindNorthPayload = 0;
+  WindEastPayload = 0;
 }
 
 SCoPEx::~SCoPEx() {}
@@ -100,7 +116,7 @@ void SCoPEx::graphicsStart() {
       (float)1.2*(2*Model.balloonRadius+Model.tetherLength),
       3,
       (float)Model.initialAltitude}; // view point [m]
-  static float hpr[3] = {-180, 20.5, 0}; // view direction[°]
+  static float hpr[3] = {-180, 20.5, 0}; // view direction[ï¿½]
   dsSetViewpoint (xyz,hpr);           // set a view point and direction
   puts("Controls:");
   puts("   f - increase thrust");
@@ -139,6 +155,8 @@ void SCoPEx::graphicsCommand(int c) {
 }
 #endif
 
+
+
 void SCoPEx::printdR3(const dReal *d) {
   printdRN(d,3);
   // fprintf(ofp, ",%7.3lf,%7.3lf,%7.3lf", (double)d[0], (double)d[1], (double)d[2]);
@@ -162,6 +180,17 @@ void SCoPEx::dBodyAddDrag(dBodyID ID, dReal Cd, dReal Area) {
   }
 }
 
+
+void SCoPEx::dBodyAddDragWind(dBodyID ID, dReal Cd, dReal Area, dReal vx, dReal vy, dReal vz) {
+  double Vs2 = vx*vx + vy*vy + vz*vz;
+  if (Vs2 > 1e-6) {
+    double Vs = sqrt(Vs2);
+    double rho_air = Pressure*100/(R_air*Temperature); // Kg/m^3
+    double Fds = 0.5*rho_air*Vs2*Cd*Area;
+    dBodyAddForce(ID, -vx*Fds/Vs, -vy*Fds/Vs, -vz*Fds/Vs);
+  }
+}
+
 dReal SCoPEx::angleDiff(dReal a1, dReal a2) {
   dReal diff = fmod(a1-a2+180,360);
   diff += (diff < 0) ? 180 : -180;
@@ -177,18 +206,111 @@ void SCoPEx::printTorque(const char *when, const dReal *torque) {
   ofp = save_fp;
 }
 
-void SCoPEx::commandStep() {
-  if (opt_commandfile) {
-    double now = tcount * stepSize;
-    while (nextCmdTime <= now) {
-      double dT = cmdfile->eval();
-      if (dT < 0) {
-        run = false;
-        break;
-      }
-      nextCmdTime = now+dT;
-    }
+void SCoPEx::parseInitfile() {
+  char line[512];
+  FILE *fp = NULL;
+
+  fp = fopen(opt_initfile,"r");
+    
+  if( fp == NULL ) {                       
+    printf("command file not found\n");
+    return;
   }
+
+
+  char varname[50];
+  char value[50];
+  while (fgets(line, 512, fp) != NULL) {
+    line[strlen(line) - 1] = '\0'; // eat the newline fgets() stores
+
+    for (int i=0;i<50;i++) {
+      varname[i] = 32;
+      value[i] = 32;
+    }
+    int delFound=0;
+    int l=0;
+    //printf("%s\n", line);
+    for (int i=0;i<strlen(line);i++) {
+      if (line[i]==32) { delFound=i; l=i; varname[i]=0; } else {
+        if (delFound==0) {varname[i]=line[i]; }
+        else {
+          if (line[i]==59) { delFound=0; } else {value[i-delFound-1]=line[i];}
+          }
+      }  
+    }
+    
+    int test=0;
+    
+    if (strcasecmp("initialAltitude",varname)==0)   { initialAltitude=std::stod(value); test=1; }
+    if (strcasecmp("ductArea",varname)==0)          { ductArea=std::stod(value); test=1; }
+    if (strcasecmp("ductCdischarge",varname)==0)    { ductCdischarge=std::stod(value); test=1; }
+    if (strcasecmp("ductHeightRatio",varname)==0)   { ductHeightRatio=std::stod(value); test=1; }
+    if (strcasecmp("payloadMass",varname)==0)       { payloadMass=std::stod(value); test=1; }
+    if (strcasecmp("thrust",varname)==0)            { thrust=std::stod(value); test=1; }
+    if (strcasecmp("dThrust",varname)==0)           { dThrust=std::stod(value); test=1; }
+    if (strcasecmp("WindNorthPayload",varname)==0)  { WindNorthPayload=std::stod(value); test=1; }
+    if (strcasecmp("WindEastPayload",varname)==0)   { WindEastPayload=std::stod(value); test=1; }
+    if (strcasecmp("WindNorthBalloon",varname)==0)  { WindNorthBalloon=std::stod(value); test=1; }
+    if (strcasecmp("WindEastBalloon",varname)==0)   { WindEastBalloon=std::stod(value); test=1; }
+    if (strcasecmp("Pressure",varname)==0)          { Pressure=std::stod(value); test=1; }
+    if (strcasecmp("Temperature",varname)==0)       { Temperature=std::stod(value); test=1; }
+    //printf("%i %s %f\n", test,varname,std::stod(value));
+  }
+  fclose(fp);
+}
+
+
+void SCoPEx::commandStep() {
+  char line[512];
+  FILE *fp = NULL;
+
+  fp = fopen(opt_commandfile,"r");
+    
+  if( fp == NULL ) {                       
+    printf("command file not found\n");
+    return;
+  }
+
+
+  char varname[50];
+  char value[50];
+  while (fgets(line, 512, fp) != NULL) {
+    line[strlen(line) - 1] = '\0'; // eat the newline fgets() stores
+
+    for (int i=0;i<50;i++) {
+      varname[i] = 32;
+      value[i] = 32;
+    }
+    int delFound=0;
+    int l=0;
+    //printf("%s\n", line);
+    for (int i=0;i<strlen(line);i++) {
+      if (line[i]==32) { delFound=i; l=i; varname[i]=0; } else {
+        if (delFound==0) {varname[i]=line[i]; }
+        else {
+          if (line[i]==59) { delFound=0; } else {value[i-delFound-1]=line[i];}
+          }
+      }  
+    }
+    
+    int test=0;
+    if (strcasecmp("initialAltitude",varname)==0)   { initialAltitude=std::stod(value); test=1; }
+    if (strcasecmp("ductArea",varname)==0)          { ductArea=std::stod(value); test=1; }
+    if (strcasecmp("ductCdischarge",varname)==0)    { ductCdischarge=std::stod(value); test=1; }
+    if (strcasecmp("ductHeightRatio",varname)==0)   { ductHeightRatio=std::stod(value); test=1; }
+    if (strcasecmp("payloadMass",varname)==0)       { payloadMass=std::stod(value); test=1; }
+    if (strcasecmp("thrust",varname)==0)            { thrust=std::stod(value); test=1; }
+    if (strcasecmp("dThrust",varname)==0)           { dThrust=std::stod(value); test=1; }
+    if (strcasecmp("WindNorthPayload",varname)==0)  { WindNorthPayload=std::stod(value); test=1; }
+    if (strcasecmp("WindEastPayload",varname)==0)   { WindEastPayload=std::stod(value); test=1; }
+    if (strcasecmp("WindNorthBalloon",varname)==0)  { WindNorthBalloon=std::stod(value); test=1; }
+    if (strcasecmp("WindEastBalloon",varname)==0)   { WindEastBalloon=std::stod(value); test=1; }
+    if (strcasecmp("Pressure",varname)==0)          { Pressure=std::stod(value); test=1; }
+    if (strcasecmp("Temperature",varname)==0)       { Temperature=std::stod(value); test=1; }
+
+    //printf("%i %s %f\n", test,varname,std::stod(value));
+  }
+  fclose(fp);
 }
 
 void SCoPEx::clamp(dReal &value, dReal abs_limit) {
@@ -215,28 +337,67 @@ void SCoPEx::Step() {
   // Drag
   dReal balloonArea = pi*balloonRadius*balloonRadius;
   // ### calculate balloonCd based on altitude, velocity
-  dBodyAddDrag(balloonID, balloonCd, balloonArea);
-  dBodyAddDrag(payloadID, payloadCd, payloadArea);
+  //MB dBodyAddDrag(balloonID, balloonCd, balloonArea);
+  //MB dBodyAddDrag(payloadID, payloadCd, payloadArea);
   // printTorque("After drag", gondolaTorque);
-  
+
+
+
+
   const dReal *newPayloadPos = dBodyGetPosition(payloadID);
-  dVector3 boxVelocity;
-  gondolaSpeed = 0;
-  // Let's just use horizontal velocity at the moment
-  for (int i = 0; i < 2; ++i) {
-    boxVelocity[i] = (newPayloadPos[i]-prevPayloadPos[i])/stepSize;
-    gondolaSpeed += boxVelocity[i]*boxVelocity[i];
-    prevPayloadPos[i] = newPayloadPos[i];
-  }
-  gondolaSpeed = sqrt(gondolaSpeed);
+  const dReal *newPayloadVelocity = dBodyGetLinearVel (payloadID);
+
+  //dVector3 boxVelocity;
+  gondolaSpeed = sqrt(newPayloadVelocity[0]*newPayloadVelocity[0] + newPayloadVelocity[1]*newPayloadVelocity[1] + newPayloadVelocity[2]*newPayloadVelocity[2]);
+  //// Let's just use horizontal velocity at the moment
+  //for (int i = 0; i < 2; ++i) {
+  //  boxVelocity[i] = (newPayloadPos[i]-prevPayloadPos[i])/stepSize;
+  //  gondolaSpeed += boxVelocity[i]*boxVelocity[i];
+  //  prevPayloadPos[i] = newPayloadPos[i];
+  //}
+  //gondolaSpeed = sqrt(gondolaSpeed);
 
   const dReal *rot = dBodyGetRotation(payloadID);
   gondolaAngle = atan2(rot[1*4+1],rot[0*4+1])*180/pi;
 
   gondolaVelocityAngle = gondolaSpeed > 0.1 ?
-    atan2(boxVelocity[1],boxVelocity[0]) * 180/pi :
+    atan2(newPayloadVelocity[1],newPayloadVelocity[0]) * 180/pi :
     gondolaAngle;
+
+  // calculate airspeeds:
+  const dReal *newBalloonPos = dBodyGetPosition(balloonID);
+  const dReal *newBalloonVelocity = dBodyGetLinearVel (balloonID);
+
+  //dVector3 boxVelocity;
+
+  dVector3 boxBalloonVelocity;
   
+  //// Let's just use horizontal velocity at the moment
+  //for (int i = 0; i < 2; ++i) {
+  //  boxBalloonVelocity[i] = (newBalloonPos[i]-prevBalloonPos[i])/stepSize;
+  //  prevBalloonPos[i] = newBalloonPos[i];
+  //}
+  dVector3 airSpeedBalloon;
+  airSpeedBalloon[0] = newBalloonVelocity[0] + WindNorthBalloon;
+  airSpeedBalloon[1] = newBalloonVelocity[1] + WindEastBalloon;
+  airSpeedBalloon[2] = newBalloonVelocity[2];
+  
+  dVector3 airSpeedPayload;
+  airSpeedPayload[0] = newPayloadVelocity[0] + WindNorthPayload;
+  airSpeedPayload[1] = newPayloadVelocity[1] + WindEastPayload;
+  airSpeedPayload[2] = newPayloadVelocity[2];
+  
+
+  //const dReal *newBalloonV = dBodyGetLinearVel (balloonID);
+  //printf("v1: %12.8lf %12.8lf\n", newBalloonV[0], newBalloonV[1]);
+  //printf("v2: %12.8lf %12.8lf\n", newBalloonVelocity[0], newBalloonVelocity[1]);
+
+
+  // Forces due to winds:
+  dBodyAddDragWind(balloonID, balloonCd, balloonArea, airSpeedBalloon[0], airSpeedBalloon[1], airSpeedBalloon[2]);
+  dBodyAddDragWind(payloadID, payloadCd, payloadArea, airSpeedPayload[0], airSpeedPayload[1], airSpeedBalloon[2]);
+  //dBodyAddDragWind(payloadID, payloadCd, payloadArea, WindNorthPayload, WindEastPayload, newBalloonVelocity[0], newBalloonVelocity[1]);
+
   
   // The outer control loop. This is configured such that
   // PGain and DGain define the inner loop, which controls the gondola
@@ -263,21 +424,27 @@ void SCoPEx::Step() {
   
   if (dThrust_a > 1) dThrust_a = 1;
   else if (dThrust_a < -1) dThrust_a = -1;
-  thrust_left = thrust * (1+dThrust_a) / 2;
-  thrust_right = thrust * (1-dThrust_a) / 2;
+  //MB thrust_left = thrust * (1+dThrust_a) / 2;
+  //MB thrust_right = thrust * (1-dThrust_a) / 2;
   
-  // printf("Thrust: %12.8lf %12.8lf\n", thrust_left, thrust_right);
+  // note: this might be switched; maybe the force is applied reversed?
+  thrust_left = thrust * (1-(dThrust+1)/2); // this expression should be left
+  thrust_right = thrust * (dThrust+1)/2;
+
+
+  //printf("Thrust: %12.8lf %12.8lf\n", thrust_left, thrust_right);
   dBodyAddRelForceAtRelPos(payloadID, 0, thrust_left, 0,
-    -payloadSize[0]/2, -payloadSize[1]/2, 0); //payloadSize[2]/2);
+    +motorAxisPosition[0], -motorAxisPosition[1], motorAxisPosition[2]); //payloadSize[2]/2);
   // printTorque("After left thrust", gondolaTorque);
   dBodyAddRelForceAtRelPos(payloadID, 0, thrust_right, 0,
-    +payloadSize[0]/2, -payloadSize[1]/2, 0); // payloadSize[2]/2);
+    -motorAxisPosition[0], -motorAxisPosition[1], motorAxisPosition[2]); // payloadSize[2]/2);
   // printTorque("After right thrust", gondolaTorque);
 
   ++tcount;
   if (tcount*stepSize > 30 && !opt_commandfile && !opt_graphics) {
     run = false;
   }
+  Log();
 }
 
 void SCoPEx::LogBody(dBodyID b) {
@@ -289,8 +456,8 @@ void SCoPEx::LogBody(dBodyID b) {
   printdRN(vec, 3);
   vec = dBodyGetTorque(b);
   printdRN(vec, 3);
-  vec = dBodyGetRotation(b);
-  printdRN(vec, 12);
+  //vec = dBodyGetRotation(b);
+  //printdRN(vec, 12);
 }
 
 void SCoPEx::LogJoint(dJointFeedback *j) {
@@ -321,18 +488,26 @@ function [B, Di] = GetBodyData(D, col)
   [Gangles,Di] = GetSimVar(D,'GondolaAngles',Di,5);
  */
 void SCoPEx::Log() {
-  if (ofp == 0) return;
+  //if (ofp == 0) return;
+  ofp = fopen(opt_logfile, "w");
   fprintf(ofp, "%7.2lf", tcount*stepSize);
+  //ofp << "test";
+  
   LogBody(payloadID);
-  LogBody(tetherID);
-  // LogBody(balloonID);
+  //LogBody(tetherID);
+  LogBody(balloonID);
   // LogJoint(&tetherPayloadFB);
+  
   fprintf(ofp,",%12.8lf,%12.8lf", thrust_left, thrust_right);
   fprintf(ofp,",%12.8lf,%12.8lf,%12.8lf,%12.8lf,%12.8lf", gondolaAngle,
           gondolaVelocityAngle, gondolaSpeed, direction, gondolaAngleSetpoint);
   fprintf(ofp,",%12.8lf,%12.8lf,%12.8lf", HeliumMass, balloonVolume,
           HeliumPressureOffset);
-  fprintf(ofp, "\n");
+  fprintf(ofp,",%12.8lf,%12.8lf", Pressure, Temperature);
+  fprintf(ofp,",%12.8lf,%12.8lf", WindNorthBalloon, WindEastBalloon);
+  fprintf(ofp,",%12.8lf,%12.8lf", WindNorthPayload, WindEastPayload);
+  fprintf(ofp, "\n"); 
+  fclose(ofp);
 }
 
 #if USE_DRAWSTUFF
@@ -377,7 +552,7 @@ void SCoPEx::calculateBuoyancy() {
   
   // Balloon Volume
   HeliumMass -= ductDischargeRate * stepSize;
-  model_atmos::get_PT(alt_km, Pressure, Temperature);
+  //MB model_atmos::get_PT(alt_km, Pressure, Temperature);
   dReal rho_he = Pressure*100/
         (R_He * (Temperature+HeliumTemperatureOffset));
   balloonVolume = HeliumMass / rho_he;
@@ -408,10 +583,13 @@ void SCoPEx::calculateBuoyancy() {
   HeliumPressureOffset = Poffset/100; // hPa
 }
 
+
+// ====================================================================================================================================
+
 void SCoPEx::Init(int argc, char **argv) {
   int c;
   opterr = 0; /* disable default error message */
-  while ((c = getopt(argc, argv, "gl:f:")) != -1) {
+  while ((c = getopt(argc, argv, "gl:f:i:")) != -1) {
     switch (c) {
       case 'g':
         opt_graphics = true;
@@ -422,18 +600,30 @@ void SCoPEx::Init(int argc, char **argv) {
       case 'f':
         opt_commandfile = optarg;
         break;
+      case 'i':
+        opt_initfile = optarg;
+        break;
+
       case '?':
         msg(3, "Unrecognized Option -%c", optopt);
     }
   }
+  if (opt_initfile) {
+    parseInitfile();
+  }
+
   if (opt_logfile) {
-    ofp = fopen(opt_logfile, "w");
+    //MB ofp = fopen(opt_logfile, "w");
+    //std::ofstream ofp(opt_logfile, std::ofstream::trunc);
   }
   if (opt_commandfile) {
     cmdfile = new commandFile(opt_commandfile);
+    cmdfile->addVariable(&Pressure, "Pressure");
+    cmdfile->addVariable(&Temperature, "Temperature");
     cmdfile->addVariable(&thrust, "Thrust");
     cmdfile->addVariable(&direction, "Direction");
     cmdfile->addVariable(&PGain, "PGain");
+    cmdfile->addVariable(&payloadMass, "PayloadMass");
     cmdfile->addVariable(&IGain, "IGain");
     cmdfile->addVariable(&DGain, "DGain");
     cmdfile->addVariable(&VPGain, "VPGain");
@@ -446,6 +636,10 @@ void SCoPEx::Init(int argc, char **argv) {
     cmdfile->addVariable(&gondolaAngleIntegralLimit,
                             "gondolaAngleIntegralLimit");
     cmdfile->addVariable(&dThrust, "dThrust");
+    cmdfile->addVariable(&WindNorthBalloon, "WindNorthBalloon");
+    cmdfile->addVariable(&WindEastBalloon, "WindEastBalloon");
+    cmdfile->addVariable(&WindNorthPayload, "WindNorthPayload");
+    cmdfile->addVariable(&WindEastPayload, "WindEastPayload");
   }
   commandStep();
   dInitODE();              // Initialize ODE
@@ -485,9 +679,18 @@ void SCoPEx::Init(int argc, char **argv) {
   dMassSetBoxTotal (&m,payloadMass,payloadSize[0],payloadSize[1],payloadSize[2]);
   dBodySetMass (payloadID,&m);
   dBodySetPosition (payloadID,0,0,payloadAltitude);
+
+  // set initial velocity to wind speed:
+  dBodySetLinearVel  (balloonID, -WindNorthBalloon, -WindEastBalloon, 0);
+  dBodySetLinearVel  (payloadID, -WindNorthPayload, -WindEastPayload, 0);
+
   prevPayloadPos[0] = 0;
   prevPayloadPos[1] = 0;
   prevPayloadPos[2] = payloadAltitude;
+  
+  prevBalloonPos[0] = 0;
+  prevBalloonPos[1] = 0;
+  prevBalloonPos[2] = balloonAltitude;
   // dMatrix3 pRot = {
      // 0, 1, 0, 0,
     // -1, 0, 0, 0,
@@ -502,7 +705,8 @@ void SCoPEx::Init(int argc, char **argv) {
 
 void SCoPEx::Close() {
   if (opt_logfile) {
-    fclose(ofp);
+    //MB fclose(ofp);
+    //ofp.close();
     ofp = 0;
   }
   dWorldDestroy (world); // destroy the world
@@ -510,6 +714,9 @@ void SCoPEx::Close() {
 }
 
 void SCoPEx::Loop() {
+  using namespace std::this_thread; // sleep_for, sleep_until
+  using namespace std::chrono; // nanoseconds, system_clock, seconds
+
   #if USE_DRAWSTUFF
   if (opt_graphics) {
     // Simulation loop
@@ -526,9 +733,17 @@ void SCoPEx::Loop() {
   #endif
     run = true;
     while (run) {
+      auto start = system_clock::now();
       Step();
-      Log();
+      sleep_until(start + nanoseconds(50000000));
+      //sleep_for(nanoseconds(50000000));
+      //Log();
     }
+  // indicate 'standby' for FlightGear:
+  ofp = fopen(opt_logfile, "w");
+  fprintf(ofp, "-1,standing by\n", tcount*stepSize);
+  fclose(ofp);
+
   #if USE_DRAWSTUFF
   }
   #endif
